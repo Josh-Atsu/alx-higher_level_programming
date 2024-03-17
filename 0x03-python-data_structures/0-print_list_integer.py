1044cb767a3c05bf0e03b2297e34d4d8f727169a
Joshua <atsujoshua67@gmail.com>
2024-03-17 13:45:25 -0700

    Print elements of a list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    counter = 0
     for i in my_list:
-        print("{}".format(my_list[counter]))
-        counter += 1
+        print("{}".format(i))
