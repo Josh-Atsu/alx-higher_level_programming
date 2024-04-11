2f765bc584b823e0fdc4bde16c3eccd8844431a0
Joshua <atsujoshua67@gmail.com>
2024-04-10 22:29:52 -0700

    Print safe numbers

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    n = 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
+            n += 1
         except IndexError:
             break
-    if i == 4:
-        i += 1
     print("")
-    return (i)
+    return (n)
