5e2033f8985d88f0e3fbf7efe8129f40289b462d
Joshua <atsujoshua67@gmail.com>
2024-04-12 06:07:45 -0700

    empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-#python3 -c 'print(__import__("my_module").MyClass.__doc__)')
+'''python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    '''
 class Square:
-    #python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    '''python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        '''
     def try_w(self):
         print("Hello, how are you")
-    #python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+    '''python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+        '''
