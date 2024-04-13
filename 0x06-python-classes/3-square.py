2279f98b302b2de98b6802be63db785b1f50a646
Joshua <atsujoshua67@gmail.com>
2024-04-13 01:42:40 -0700

    A class that defines an square and return the area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    """
+
+
+class Square:
+    """python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        """
+    def __init__(self, size=0):
+        if size is not int(size):
+            raise TypeError("size must be an integer")
+            return
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+            return
+        self.__size = size
+        """python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+            """
+
+    """python3 -c 'print(__import__("my_module").my_function.__doc__)'
+        """
+    def area(self):
+        area_s = self.__size ** 2
+        return (area_s)
+        """python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
+            """
