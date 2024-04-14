b3389e34f4712a84c618644806a36087ef44889b
Joshua <atsujoshua67@gmail.com>
2024-04-14 00:40:34 -0700

    a Square class for my_print method with size and position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -57,7 +57,7 @@ class Square:
             print()
         else:
             for i in range(0, n):
-                for y in range(0, self.__position[1]):
+                for z in range(0, self.__position[1]):
                     print("", end="")
                 for y in range(0, self.__position[0]):
                     print(" ", end="")
