20cd0f786f3f0941c3bba120f90c38a07c8fe9ac
Joshua <atsujoshua67@gmail.com>
2024-05-11 15:07:00 -0700

    python TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,10 @@
 
 def add_integer(a, b=98):
     """Adds two integers"""
+    #if not isinstance(a, int):
+        #raise TypeError("a must be an integer")
+    if isinstance(a, float):
+        a = int(a)
     if not isinstance(a, int):
         raise TypeError("a must be an integer")
     return a + b
