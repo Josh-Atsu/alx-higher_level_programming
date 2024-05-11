4ad89893f5728baf2319eb0dd4a37b4b752933bf
Joshua <atsujoshua67@gmail.com>
2024-05-11 01:24:19 -0700

    N Queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -13,7 +13,7 @@ if len_arg != 2:
     print("Usage: nqueens N")
     sys.exit(1)
 
-if int(sys.argv[1]) is False:
+if sys.argv[1].isdigit() is False:
     print("N must be a number")
     sys.exit(1)
 
