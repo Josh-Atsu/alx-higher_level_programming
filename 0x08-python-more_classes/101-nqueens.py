48c273304c035f425f6b081b892fbfc450ce0795
Joshua <atsujoshua67@gmail.com>
2024-05-11 01:09:03 -0700

    N Queens

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# N queens
+"""
+A The N queens puzzle is the challenge of placing
+N non-attacking queens on an N×N chessboard
+
+a program that solves the N queens problem."""
+
+import sys
+
+len_arg = len(sys.argv)
+if len_arg != 2:
+    print("Usage: nqueens N")
+    exit(1)
+
+if not sys.argv[1].isdigit:
+    print("N must be a number")
+    exit(1)
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
