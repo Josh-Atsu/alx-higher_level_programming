4cd13d4cf54a43864144bd0c5ee5771403399773
Joshua <atsujoshua67@gmail.com>
2024-05-11 14:44:38 -0700

    python TDD

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# A function that adds two numbers
+"""Adds two numbers"""
+
+
+def add_integer(a, b=98):
+    """Adds two integers"""
+    if not isinstance(a, int):
+        raise TypeError("a must be an integer")
+    return a + b
