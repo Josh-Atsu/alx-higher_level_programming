5e892f06d4ed3c36796ed042d0440d39694f2723
Joshua <atsujoshua67@gmail.com>
2024-05-11 17:08:59 -0700

    python TDD

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# A function that divides the elemens of a matrix
+"""Divides Element Of a matrix"""
+
+def matrix_divided(matrix, div):
+    """Divides element of a matrix
+    args:
+      matrix: list representation containing integers or float
+      div: number to be divided by"""
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+    #if div == 0:
+        #raise ZeroDivisionError("division by zero")
+    my_array = []
+    for array in matrix:
+        inner_array = []
+        for i in array:
+            num = i / div
+            inner_array.append(num)
+        my_array.append(inner_array)
+    return my_array
