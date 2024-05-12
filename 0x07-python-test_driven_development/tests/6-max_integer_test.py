7322a73bfaa2a77c2c37e1fc5ea76c3206a2fb66
Joshua <atsujoshua67@gmail.com>
2024-05-12 08:58:04 -0700

    Python TDD

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,9 +11,21 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_max_integer(self):
         max1 = max_integer([1, 2, 3, 4])
-        max2 = max_integer([1, 2, 4, 3])
+        max2 = max_integer([1, 9, 4, 3])
         self.assertEqual(max1, 4)
+        self.assertEqual(max2, 9)
+
+    def test_negative(self):
+        max2 = max_integer([1, -9, 4, 3])
         self.assertEqual(max2, 4)
+        max1 = max_integer([-30, -8, -1])
+        self.assertEqual(max1, -1)
+
+    def test_exist(self):
+        max = max_integer([2])
+        self.assertIs(max)
+        max_n = max_integer()
+        self.assertIsNone(max_n)
 
 if __name__ == '__main__':
     unittest.main()
