977640675ed3a01a894265de80ffa2147a030e09
Joshua <atsujoshua67@gmail.com>
2024-05-13 06:58:35 -0700

    return available attributes and methods

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 # A function that returns the list of available class
+"""A function that returns the list of available class"""
+
+
 def lookup(obj):
+    """Return dir"""
     return dir(obj)
