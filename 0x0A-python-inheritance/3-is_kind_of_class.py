f7bf20ec274fcb5caa14229cf43f7696bd7eb482
Joshua <atsujoshua67@gmail.com>
2024-05-13 10:58:48 -0700

    isinstance of another

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This is a file that defines a funtion to check
+if one is an instance of another"""
+
+
+def is_kind_of_class(obj, a_class):
+    """The function check if isinstance of"""
+    if isinstance(abl, a_class):
+        return True
+    else:
+        return False
