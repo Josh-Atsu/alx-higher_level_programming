f7bde9f64c0ff2c1523d3384044c2915433f001c
Joshua <atsujoshua67@gmail.com>
2024-05-13 11:05:03 -0700

    isinstance of another

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function returns True if the object is an instance of a class
+that inherited (directly or indirectly) from the specified class
+; otherwise False."""
+
+
+def inherits_from(obj, a_class):
+    """check if is a subclass of using issubclass"""
+    if issubclass(obj, a_class):
+        return True
+    else:
+        return False
