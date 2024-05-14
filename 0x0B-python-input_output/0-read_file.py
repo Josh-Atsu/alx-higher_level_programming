23b19b8be51aab94db215461bed56e8bf5ba3d57
Joshua <atsujoshua67@gmail.com>
2024-05-13 22:43:29 -0700

    Python i/o

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ and prints it to stdout:"""
 def read_file(filename=""):
     """Opens filename, read and print to stdout"""
     with open(filename, encoding="utf-8") as myfile:
-        for line in myfile.readline():
+        for line in myfile.readlines():
             print("{}".format(line))
