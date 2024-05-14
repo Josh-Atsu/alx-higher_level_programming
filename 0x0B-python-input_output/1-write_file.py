3a2aa4c664c442746880542f771c1ed6e1c725ee
Joshua <atsujoshua67@gmail.com>
2024-05-14 00:10:42 -0700

    Python i/o

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-# a function that writes a string to a text file (UTF8)
 """
 a function that writes a string to a text file (UTF8)
  and returns the number of characters written
  args:
      filename: file to write into
      text: contains the string to write
-     """
+"""
 
 
 def write_file(filename="", text=""):
     """Use the permission write to write
     and overite the file if it exist"""
-
     with open(filename, "w" encoding="utf-8") as myfile:
         CharNum = myfile.write(text)
         return CharNum
