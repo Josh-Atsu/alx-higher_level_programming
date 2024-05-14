56cd67b66640fb91bb0579b15980ee9c4dec811f
Joshua <atsujoshua67@gmail.com>
2024-05-14 03:27:49 -0700

    Python i/o

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Defines a script that adds all arguments to a Python list,
 and then save them to a file:"""
+
+import sys, json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-import sys
-import json
 
 py_list = []
 for i in sys.argv:
