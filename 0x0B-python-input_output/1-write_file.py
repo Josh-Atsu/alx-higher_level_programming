ac915c8105314e15cf6dfd23de7bde16b1ea9994
Joshua <atsujoshua67@gmail.com>
2024-05-14 00:38:16 -0700

    Python i/o

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 # 1-write_file.py
-"""Defines a file writing into a file."""
+"""Defines a function writing into a file."""
 
 
 def write_file(filename="", text=""):
     """
     writes a string to a text file (UTF8)
     and returns the number of characters written
-    args:
+    Args:
      filename: file to write into
      text: contains the string to write
      """
+
     with open(filename, "w" encoding="utf-8") as myfile:
         CharNum = myfile.write(text)
         return CharNum
