c7e93d2544cff66f1086d96bc2e7a8db9abe44dc
Joshua <atsujoshua67@gmail.com>
2024-05-13 23:24:54 -0700

    Python i/o

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,9 +3,11 @@
 and returns the number of characters written """
 
 
+
 def write_file(filename="", text=""):
     """Use the permission write to write
     and overite the file if it exist"""
+
     with open(filename, "w" encoding="utf-8") as myfile:
         CharNum = myfile.write(text)
         return CharNum
