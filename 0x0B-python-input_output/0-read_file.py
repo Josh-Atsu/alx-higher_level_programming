63111d7c3ff3195837a3374d6f6b4f0d28958e24
Joshua <atsujoshua67@gmail.com>
2024-05-13 22:40:30 -0700

    Python i/o

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a function that reads a text file (UTF8)
+and prints it to stdout:"""
+
+
+def read_file(filename=""):
+    """Opens filename, read and print to stdout"""
+    with open("filename", encoding="utf-8") as myfile:
+        for line in myfile.readline():
+            print("{}".format(line))
