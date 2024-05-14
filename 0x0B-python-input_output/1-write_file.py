2c59765fc89030a539971f9c2423e92c7251bdf8
Joshua <atsujoshua67@gmail.com>
2024-05-13 23:07:55 -0700

    Python i/o

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#1/usr/bin/python3
+"""a function that writes a string to a text file (UTF8)
+and returns the number of characters written:"""
+
+
+def write_file(filename="", text=""):
+    """Use the permission write to write
+    and overite the file if it exist"""
+    with open(filename, encoding="utf-8") as myfile:
+        my_read_file = myfile.read()
+
+    with open(text, "w", encoding="utf-8") as my_file:
+        writeChar = my_file.write(my_read_file)
+        return writeChar
+
