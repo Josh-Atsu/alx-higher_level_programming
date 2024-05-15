897dcb9823bef3dfd7e2e4d4fbcd62f0c71e3778
Joshua <atsujoshua67@gmail.com>
2024-05-15 03:44:19 -0700

    Python i/o

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 # Json serialization
 """JSON Serialization"""
-import json
 
 
 def class_to_json(obj):
     """convert to json string and return the dictionary rep"""
-    return json.dumbs(obj)
+    return obj.__dict__
