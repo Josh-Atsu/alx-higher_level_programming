c60a781bbd07ed268188f59607da4e43a62beb05
Joshua <atsujoshua67@gmail.com>
2024-05-16 16:41:17 -0700

    Square inheritance

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,20 @@ class Square(Rectangle):
         """Class constructor"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter property and get attributes"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set values"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value <= 0:
+            raise ValueError("size be must > 0")
+        self.__size = value
+
     def __str__(self):
         """Overload __str__ and
         return [Square] (<id>) <x>/<y> - <size>"""
