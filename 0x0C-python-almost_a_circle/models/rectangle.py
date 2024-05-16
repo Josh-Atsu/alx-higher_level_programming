d848e2331a05d39573d696a0c8d8d9dcea97dd0e
Joshua <atsujoshua67@gmail.com>
2024-05-16 11:18:02 -0700

    Rectangle class inheritance

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,4 +86,5 @@ class Rectangle(Base):
         [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                     self.__x, self.__y,
-                                                    self.__width, self.__height)
+                                                    self.__width,
+                                                    self.__height)
