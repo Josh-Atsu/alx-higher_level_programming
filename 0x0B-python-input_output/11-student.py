870a2724377e8bbe890621f0474da9801c8dd552
Joshua <atsujoshua67@gmail.com>
2024-05-16 04:00:05 -0700

    Python i/o

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -27,6 +27,8 @@ class Student:
 
     def reload_from_json(self, json):
         """replaces all attributes of the Student instance"""
+        if json == None:
+            return
         self.first_name = json["first_name"]
         self.last_name = json["last_name"]
         self.age = json["age"]
