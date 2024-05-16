faee86620168ac4eb101d3929fdfbee2217b9386
Joshua <atsujoshua67@gmail.com>
2024-05-16 06:35:17 -0700

    Rectangle inheritance class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle(Base):
     """Class Rectangle that inherits from base"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """Class constructor"""
         self.width = width
@@ -14,32 +15,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Getter property and get attributes"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set values"""
         self.__width = value
 
     @property
     def height(self):
+        """Getter property and get attributes"""
         return self.__height
-
+    
     @height.setter
     def height(self, value):
+        """Set values"""
         self.__height = value
 
     @property
     def x(self):
+        """Getter property and get attributes"""
         return self.__x
-
+    
     @x.setter
     def x(self, value):
+        """Set values"""
         self.__x = value
 
     @property
     def y(self):
+        """Getter property and get attributes"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """Set values"""
         self.__y = value
