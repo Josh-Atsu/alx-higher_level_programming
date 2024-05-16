1cf55bff1d80799c361c5d0e212dbb67e01e83d1
Joshua <atsujoshua67@gmail.com>
2024-05-16 07:39:43 -0700

    Rectangle inheritance class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,5 +84,5 @@ class Rectangle(Base):
     def __str__(self):
         """Overide the __str__ method by returning
         [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format
+        return "[Rectangle] ({}) {}/{} - {}/{}".format\
             (self.id, self.__x, self.__y, self.__width, self.__height)
