c4f7c1fdda805433feb0678bae119431e642c1a1
Joshua <atsujoshua67@gmail.com>
2024-05-16 07:16:22 -0700

    Rectangle inheritance class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,5 +79,4 @@ class Rectangle(Base):
         for i in range(self.__height):
             for j in range(self.__width):
                 print("#", end="")
-            if i < self.__height - 1:
-                print()
+            print()
