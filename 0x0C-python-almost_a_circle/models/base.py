0449bcfae1970e8397ac532cbf20b01a78b1b115
Joshua <atsujoshua67@gmail.com>
2024-05-17 14:26:52 -0700

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,6 +38,6 @@ class Base:
     def from_json_string(json_string):
         """that returns the list of the JSON string
         representation json_string"""
-        if json_string i None or json_string == []:
+        if json_string is None or json_string == []:
             return []
         return json.loads(json_string)
