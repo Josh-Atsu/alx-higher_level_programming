32bfb4825a40730c20a45b930484e71fef55669c
Joshua <atsujoshua67@gmail.com>
2024-05-23 12:32:42 -0700

    Create new database and user and grant select privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,5 @@
 -- password should be set to user_0d_2_pwd
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 USE hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd_@Jsu';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbth_0d_2.* TO 'user_0d_2'@'localhost';
