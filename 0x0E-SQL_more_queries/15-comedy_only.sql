7dcb5d61616f555e30af5c52665e0857b36a2178
Joshua <atsujoshua67@gmail.com>
2024-05-23 22:54:54 -0700

    ists all Comedy shows in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- lists all comedy shows contained in hbtn_0d_tvshows
-SELECT tv_shows.title AS 'name'
+SELECT tv_shows.title AS 'title'
 FROM tv_show_genres
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
