345903b9c7840726810e26fa17db1c18af44d1fe
Joshua <atsujoshua67@gmail.com>
2024-06-18 19:14:43 -0700

    command line arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,11 +2,9 @@
 
 const num = process.argv.length;
 if (num === 2) {
-	console.log('No arguments')
+  console.log('No argument');
+} else if (num === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-else if (num === 3) {
-	console.log('Argument found')
-}
-else {
-	console.log('Arguments found')
-};
