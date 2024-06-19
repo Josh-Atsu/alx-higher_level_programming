c6c881521b07013a975d11857f3ff07bcc9bb165
Joshua <atsujoshua67@gmail.com>
2024-06-19 02:46:56 -0700

    sort and get second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+let myList = [];
+len = process.argv.length
+if (len === 2 || len === 3) {
+  console.log(0);
+}
+for (let i = 0; i < (len - 2); i++) {
+  myList[i] = Number(process.argv[i + 2]);
+}
+myList.sort((a, b) => b - a);
+console.log(myList[1]);
