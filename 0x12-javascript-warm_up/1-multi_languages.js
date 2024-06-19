98546ac1209e14f47b58e14e3683227dff7e50d8
Joshua <atsujoshua67@gmail.com>
2024-06-18 17:44:25 -0700

    print multi-lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 
-let myVar = 'C is fun';
-let myVar1 = 'Python is cool';
-let myVar2 = 'JavaScript is amazing';
-console.log(myVar);
-console.log(myVar1);
-console.log(myVar2);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
