6dfdb1180c41eb4e89f92ea67a33bcbc0412a030
Joshua <atsujoshua67@gmail.com>
2024-06-19 21:15:41 -0700

    reverse list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const mylist = [];
-  const l_len = list.length; let j = 0;
-  for (let i = l_len - 1; i >= 0; i--) {
+  const len = list.length; let j = 0;
+  for (let i = len - 1; i >= 0; i--) {
     mylist[j] = list[i]; j++;
   }
   return (mylist);
