7a4e11c491928f3b85c9f4630422ca6f9a9be3d4
Joshua <atsujoshua67@gmail.com>
2024-06-19 22:24:53 -0700

    sorted by Ids and occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,7 +3,7 @@ const dict = require('./101-data.js').dict;
 const newDict = {};
 for (const Ids in dict) {
   const Occ = dict[Ids];
-  if (!newDict[Occ]) newDict[Occ] = []
-  newDict[Occ].push(Ids)
-};
+  if (!newDict[Occ]) newDict[Occ] = [];
+  newDict[Occ].push(Ids);
+}
 console.log(newDict);
