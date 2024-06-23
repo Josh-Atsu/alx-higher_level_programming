cb694b84509248c1eb20b0cd3e228c017b6714c2
Joshua <atsujoshua67@gmail.com>
2024-06-23 11:38:02 -0700

    select states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""List all states in database"""
+"""List all states in databas and select according to user input"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd="", db=sys.argv[3], port=3306)
+                         passwd="sys.argv[2]", db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC"
-            .format(sys.argv[4]))
+                .format(sys.argv[4]))
     res = cur.fetchall()
     for row in res:
         print(row)
