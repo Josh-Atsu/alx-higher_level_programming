231552f0f08074b7e6f4a4c445ad58d3d63d0ae5
Joshua <atsujoshua67@gmail.com>
2024-06-23 22:48:43 -0700

    all task completed

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Define a class States that uses SQLAlchemy"""
+Define a class City that uses SQLAlchemy
+"""
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,8 +10,8 @@ Base = declarative_base(metadata=mymetadata)
 
 
 class City(Base):
-    """class with declarative base"""
+    """Defines a table name cities"""
     __tablename__ = "cities"
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
+    state_id = Column(Integer, nullable=False, ForeignKey("states.id")
