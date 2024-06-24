e48cf0924bc27e7a1037c97c8125858158db3900
Joshua <atsujoshua67@gmail.com>
2024-06-23 22:57:21 -0700

    all task completed

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,11 +2,8 @@
 """
 Define a class City that uses SQLAlchemy
 """
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
 
 
 class City(Base):
