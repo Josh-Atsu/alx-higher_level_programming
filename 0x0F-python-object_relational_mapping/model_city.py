7e33a2aba48c7dd185bb9f64fb82627c90b7c04c
Joshua <atsujoshua67@gmail.com>
2024-06-23 23:01:59 -0700

    all task completed

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,6 +2,7 @@
 """
 Define a class City that uses SQLAlchemy
 """
+from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
