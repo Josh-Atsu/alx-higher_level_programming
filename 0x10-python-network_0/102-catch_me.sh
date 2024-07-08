dce2b82ed0e4a41a956066f9450667697ddf0489
Joshua <atsujoshua67@gmail.com>
2024-07-08 15:26:13 -0700

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # server respond with the message 'you got me'
-curl -sL -X PUT -d "user_id=98" -H "Origin: School" "$1"
+curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" "$1"
