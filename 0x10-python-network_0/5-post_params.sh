60de3d792778bbf09e6461f75d0b06c6218e5199
Joshua <atsujoshua67@gmail.com>
2024-07-08 09:33:50 -0700

    Post request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a post request, display the body response message
-curl -sX "POST" -d "email=test@gmail.com7&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
