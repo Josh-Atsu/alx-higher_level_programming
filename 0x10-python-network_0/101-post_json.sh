a8d2d59685e65e0407a8df9ecb36f8b6178f9227
Joshua <atsujoshua67@gmail.com>
2024-07-08 14:34:41 -0700

    json file POSTs

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a POST request of a JSON file, and display all body response message
-curl -sX POST -d "$2" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1" 
