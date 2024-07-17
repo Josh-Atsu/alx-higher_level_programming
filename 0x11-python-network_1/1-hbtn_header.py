97adcef3c15ff94b9554a374fd81d4114d378599
Joshua <atsujoshua67@gmail.com>
2024-07-17 15:02:02 -0700

    get X-Request-Id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """send request and display the value of X-Request-Id
 """
-import urllib.request, sys
+import urllib.request
+import sys
 
 with urllib.request.urlopen(sys.argv[1]) as response:
     html = response.read()
