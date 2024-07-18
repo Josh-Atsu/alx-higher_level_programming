9019f3599705f4fb9a715fbc17cae7a98e6a5458
Joshua <atsujoshua67@gmail.com>
2024-07-18 07:28:29 -0700

    import and use requests only

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""a script that https://alx-intranet.hbtn.io/status"""
+import requests
+
+res = requests.get('https://alx-intranet.hbtn.io/status')
+r = res.text
+print("Body response:\n\t- type {}\n\t- content: {}".format(type(r), r))
