75ed9a6648ab09a5e455f6e7f1e9177b08de64e1
Joshua <atsujoshua67@gmail.com>
2024-08-01 19:08:21 -0700

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-cmd_first = process.argv[2];
+
 const fs = require('fs');
-fs.readFile(cmd_first, 'utf8', (err, data) => {
-if (err) {
-console.log(err)
-} else {
-console.log(data)
-}
+
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(data)
+  }
 });
