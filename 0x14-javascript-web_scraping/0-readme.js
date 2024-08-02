74d1a8cdd66099fb725a13345feb4ed384702fd6
Joshua <atsujoshua67@gmail.com>
2024-08-01 22:44:55 -0700

    fixed semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
-    console.log(err)
+    console.log(err);
   } else {
-    console.log(data)
+    console.log(data);
   }
 });
