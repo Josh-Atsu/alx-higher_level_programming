39878f10dcafd744641128712b66b71e9402bf70
Joshua <atsujoshua67@gmail.com>
2024-08-02 20:09:24 -0700

    copy webpage to file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    fs.writeFile(file, body, 'utf-8', (err) => {
+      if (err) {
+        console.log(err)
+      }
+    });
+  } else {
+    console.log('An Error occured. Status Code: ' + response.statusCode)
+  }
+});
